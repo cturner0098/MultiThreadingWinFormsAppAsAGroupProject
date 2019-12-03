c877d5ff83a2c6eb04cb417bb05d9566bcb99663
richa2aw <richa2aw@MCD210-PC-06>
2019-12-03 16:09:09 -0500

    commit

---
+++ MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/ClimbingCougar.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+
+namespace MultiThreadingWinFormsAppAsAGroupProject
+{
+    /// <summary>
+    /// A Fantastic Beast that will help us maintain a secure world
+    /// </summary>
+    class ClimbingCougar : FantasticBeast
+    {
+        public override void SayHello()
+        {
+            throw new NotImplementedException();
+        }
+        public override void RunThread()
+        {
+            response = maxfactor(request);
+        }
+        static private long maxfactor(long n)
+        {
+            long k = 2;
+            while (k * k <= n)
+            {
+                if (n % k == 0)
+                {
+                    n /= k;
+                }
+                else
+                {
+                    ++k;
+                }
+            }
+
+            return n;
+        }
+    }
+}
