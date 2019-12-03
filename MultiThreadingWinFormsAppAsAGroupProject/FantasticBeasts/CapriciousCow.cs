8fc588effb111dacca34236336f2a5e9eacb47e8
Christian Turner <cturner0098@gmail.com>
2019-12-03 16:06:52 -0500

    turne2cn CapriciousCow

---
+++ MultiThreadingWinFormsAppAsAGroupProject/FantasticBeasts/CapriciousCow.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+using System.Threading;
+
+namespace MultiThreadingWinFormsAppAsAGroupProject
+{
+    /// <summary>
+    /// A Fantastic Beast that will help us maintain a secure world
+    /// </summary>
+    class CapriciousCow : FantasticBeast
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Hello from " + this.GetType());
+        }
+
+        public override void RunThread()
+        {
+            response = ComputeLargestPrimeFactor(Convert.ToInt64(request)).ToString();
+            Console.WriteLine("Response: {0}", response);
+        }
+
+        public long ComputeLargestPrimeFactor(long num)
+        {
+            // declare i to be used in return
+            int i;
+            for (i = 2; i < num; i++)
+            {
+                // check if it can be divided (not prime)
+                if (num % i == 0)
+                {
+                    // if not prime, divide num by i
+                    num /= i;
+                    // minus i incase the next number is also divisible by current
+                    i--;
+                }
+            }
+
+            // return largest prime factor
+            return i;
+        }
+    }
+}
